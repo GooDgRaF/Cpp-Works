c1047c156c806ddb88e19d45d07ab41793fd84fb
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2021-04-07 16:27:30 +0500

    Update Lab_4.cpp

--- UrFU/Stochastic/Labs/Lab_4/Lab_4.cpp
+++ UrFU/Stochastic/Labs/Lab_4/Lab_4.cpp
@@ -21,71 +21,71 @@ void do_lab_4()
         double h = 0.01;
         {
 //Построить реализцаии цветного шума
-//        {
-//            std::vector<double> sample = {};
-//            std::vector<int> N_set = {};
-//            std::map<double, std::vector<double>> a_to_E = {}, a_to_V = {};
-//            for (int T = 1; T <= 100000; T *= 10)
-//            {
-//                double N = T / h;
-//                N_set.push_back(N);
-//                sample.reserve(N);
-//
-//                for (int a = 2; a <= 16; a *= 2)
-//                {
-//                    sample.clear();
-//                    double x = 0;
-//                    for (int n = 0; n < N; ++n)
-//                    {
-//                        x = colorNoise(x, h, a);
-//                        sample.push_back(x);
-//                    }
-//
-////Матожидание и Дисперсия
-//                    a_to_E[a].push_back(calc_Expectation(sample));
-//                    a_to_V[a].push_back(calc_Variance(sample));
-//
-//                    std::ostringstream a_str, T_str;
-//                    a_str << a;
-//                    T_str << T;
-//
-//                    std::string path = "../Lab_4/data/Source/sampleNoise_a" + a_str.str() + "_T" + T_str.str() + ".txt";
-//                    write_vector_in_file(sample, path);
-//                }
-//            }
-//
-//            write_data(N_set, a_to_E, "E");
-//            write_data(N_set, a_to_V, "V");
-//        }
-        }
-//Исследование корреляции между моментом T = 100 и T = 110 шагом N = 100
         {
             std::vector<double> sample = {};
-            sample.reserve(10);
-
-            for (int N = 10e1; N < 10e6; N*=10)
+            std::vector<int> N_set = {};
+            std::map<double, std::vector<double>> a_to_E = {}, a_to_V = {};
+            for (int T = 1; T <= 1000; T *= 10)
             {
-                std::ostringstream N_str;
-                N_str << N;
-                std::string path = "../Lab_4/data/Source/sample10_N" + N_str.str() + ".txt";
-                write_vector_in_file(std::vector<double>{}, path, true); //Почистили файл перед записью
-                for (int n = 0; n < N; ++n)
+                double N = T / h;
+                N_set.push_back(N);
+                sample.reserve(N);
+
+                for (int a = 64; a <= 256; a *= 2)
                 {
                     sample.clear();
                     double x = 0;
-
-                    x = skipT(10, h, x);
-                    //x = skipT(100, h, x);
-                    sample.push_back(x);
-                    for (int i = 0; i < 9; ++i)
+                    for (int n = 0; n < N; ++n)
                     {
-                        x = skipT(1, h, x);
+                        x = colorNoise(x, h, a);
                         sample.push_back(x);
                     }
-                    write_vector_in_file(sample, path, false);
+
+//Матожидание и Дисперсия
+                    a_to_E[a].push_back(calc_Expectation(sample));
+                    a_to_V[a].push_back(calc_Variance(sample));
+
+                    std::ostringstream a_str, T_str;
+                    a_str << a;
+                    T_str << T;
+
+                    std::string path = "../Lab_4/data/Source/sampleNoise_a" + a_str.str() + "_T" + T_str.str() + ".txt";
+                    write_vector_in_file(sample, path);
                 }
             }
+
+            write_data(N_set, a_to_E, "E");
+            write_data(N_set, a_to_V, "V");
         }
+        }
+//Исследование корреляции между моментом T = 100 и T = 110 шагом N = 100
+//        {
+//            std::vector<double> sample = {};
+//            sample.reserve(10);
+//
+//            for (int N = 10e1; N < 10e6; N*=10)
+//            {
+//                std::ostringstream N_str;
+//                N_str << N;
+//                std::string path = "../Lab_4/data/Source/sample10_N" + N_str.str() + ".txt";
+//                write_vector_in_file(std::vector<double>{}, path, true); //Почистили файл перед записью
+//                for (int n = 0; n < N; ++n)
+//                {
+//                    sample.clear();
+//                    double x = 0;
+//
+//                    x = skipT(10, h, x);
+//                    //x = skipT(100, h, x);
+//                    sample.push_back(x);
+//                    for (int i = 0; i < 9; ++i)
+//                    {
+//                        x = skipT(1, h, x);
+//                        sample.push_back(x);
+//                    }
+//                    write_vector_in_file(sample, path, false);
+//                }
+//            }
+//        }
 
     }
 
