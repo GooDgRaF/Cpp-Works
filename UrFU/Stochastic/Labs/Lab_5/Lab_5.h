7a154d38d7977037927cfdf7479b5062ae822828
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2021-04-11 12:51:58 +0500

    Лаба (5 0/1) - написал РК-4

---
+++ UrFU/Stochastic/Labs/Lab_5/Lab_5.h
@@ -0,0 +1,10 @@
+//
+// Created by Антон on 10.04.2021.
+//
+
+#ifndef LABA1_LAB_5_H
+#define LABA1_LAB_5_H
+
+void do_lab_5();
+
+#endif //LABA1_LAB_5_H
--- UrFU/Stochastic/Labs/main.cpp
+++ UrFU/Stochastic/Labs/main.cpp
@@ -5,6 +5,7 @@
 #include "Lab_2/Lab_2.h"
 #include "Lab_3/Lab_3.h"
 #include "Lab_4/Lab_4.h"
+#include "Lab_5/Lab_5.h"
 #include "Support/Random_engine.h"
 #include "Support/Calc_Expectation_Variance_Covariance.h"
 
@@ -17,7 +18,8 @@ int main()
         //do_lab_1(random_generator);
         //do_lab_2(random_generator);
         //do_lab_3(random_generator);
-        do_lab_4();
+        //do_lab_4();
+        do_lab_5();
 
 
 
