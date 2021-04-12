90a3430a25c8bc03fc52c61ee7fa3dddd05df909
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2021-04-12 18:56:15 +0500

    Update Lab_5.cpp

--- UrFU/Stochastic/Labs/Lab_5/Lab_5.cpp
+++ UrFU/Stochastic/Labs/Lab_5/Lab_5.cpp
@@ -12,7 +12,7 @@
 #include "Lab_5.h"
 #include "../Support/Write_functions.h"
 
-#define PARAMETER_a 0.002
+#define PARAMETER_a 1.1
 
 using namespace std;
 
@@ -22,9 +22,17 @@ void odeRC4(vector<vector<double>> &X, vector<double> &grid,
             const double T, const double h = 0.01);
 
 void
-calc_K(const vector<double> &grid, const vector<function<double(double, vector<double>)>> &F, const double h, vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<double> &k4, int t, const vector<double> &X_prev);
+calc_K(const vector<double> &grid, const vector<function<double(double, vector<double>)>> &F,
+       const double h, const int t, vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<double> &k4,
+       const vector<double> &X_prev);
 
 void clear_K(vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<double> &k4);
+
+void
+calc_k(const vector<function<double(double, vector<double>)>> &F, vector<double> &k, double t, const vector<double> &X);
+
+void
+reserve_K(const vector<double> &X_init, vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<double> &k4);
 
 vector<double> operator+(const vector<double> &a, const vector<double> &b)
     {
@@ -51,22 +59,22 @@ vector<double> operator*(const T n, const vector<double> &a)
     }
 
 
-double g1(const double t, const vector<double> &v)
+double g1(const double t, const vector<double> &X)
     {
-        return (v[0] - v[1] - v[0] * v[0] * v[0] / 3) / 10;
+        return (X[0] - X[1] - X[0] * X[0] * X[0] / 3) * 10;
     }
 
-double g2(const double t, const vector<double> &v)
+double g2(const double t, const vector<double> &X)
     {
-        return v[0] + PARAMETER_a;
+        return X[0] + PARAMETER_a;
     }
 
 void do_lab_5()
     {
         vector<vector<double>> X;
         vector<double> grid;
-        double t_init = 0, T = 500;
-        vector<double> X_init = {0, 0};
+        double t_init = 0, T = 100;
+        vector<double> X_init = {-PARAMETER_a, -1};//TODO Писать в файл с разными начальными условиями
 
         odeRC4(X, grid, {g1, g2}, t_init, X_init, T);
 
@@ -93,13 +101,15 @@ void odeRC4(vector<vector<double>> &X, vector<double> &grid,
             const double T, const double h)
     {
         double N = T / h;
+
+        X.clear();
+        grid.clear();
+
         X.reserve(N + 1);
         grid.reserve(N + 1);
+
         vector<double> k1 = {}, k2 = {}, k3 = {}, k4 = {};
-        k1.reserve(X_init.size() + 1);
-        k2.reserve(X_init.size() + 1);
-        k3.reserve(X_init.size() + 1);
-        k4.reserve(X_init.size() + 1);
+        reserve_K(X_init, k1, k2, k3, k4);
 
         grid.push_back(t_init);
         X.push_back(X_init);
@@ -110,9 +120,18 @@ void odeRC4(vector<vector<double>> &X, vector<double> &grid,
             vector<double> &X_prev = X[t - 1];
 
             clear_K(k1, k2, k3, k4);
-            calc_K(grid, F, h, k1, k2, k3, k4, t, X_prev);
+            calc_K(grid, F, h, t, k1, k2, k3, k4, X_prev);
             X.push_back(X_prev + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4));
         }
+    }
+
+void
+reserve_K(const vector<double> &X_init, vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<double> &k4)
+    {
+        k1.reserve(X_init.size() + 1);
+        k2.reserve(X_init.size() + 1);
+        k3.reserve(X_init.size() + 1);
+        k4.reserve(X_init.size() + 1);
     }
 
 void clear_K(vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<double> &k4)
@@ -125,24 +144,21 @@ void clear_K(vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<
 
 void
 calc_K(const vector<double> &grid, const vector<function<double(double, vector<double>)>> &F,
-       const double h, vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<double> &k4,
-       int t, const vector<double> &X_prev)
+       const double h, const int t, vector<double> &k1, vector<double> &k2, vector<double> &k3, vector<double> &k4,
+       const vector<double> &X_prev)
+    {
+        calc_k(F, k1, grid[t - 1], X_prev);
+        calc_k(F, k2, grid[t - 1] + h / 2, X_prev + h / 2 * k1);
+        calc_k(F, k3, grid[t - 1] + h / 2, X_prev + h / 2 * k2);
+        calc_k(F, k4, grid[t - 1] + h, X_prev + h * k3);
+    }
+
+void
+calc_k(const vector<function<double(double, vector<double>)>> &F, vector<double> &k, double t, const vector<double> &X)
     {
         for (const auto &f : F)
         {
-            k1.push_back(f(grid[t - 1], X_prev));
-        }
-        for (const auto &f : F)
-        {
-            k2.push_back(f(grid[t - 1] + h / 2, X_prev + h / 2 * k1));
-        }
-        for (const auto &f : F)
-        {
-            k3.push_back(f(grid[t - 1] + h / 2, X_prev + h / 2 * k2));
-        }
-        for (const auto &f : F)
-        {
-            k4.push_back(f(grid[t - 1] + h, X_prev + h * k3));
+            k.push_back(f(t, X));
         }
     }
 
