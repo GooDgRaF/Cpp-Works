d2a30e48d3fe09747136a0bb61ab03b754054aff
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2021-04-27 22:18:15 +0500

    Переезд на шаблоны

--- UrFU/Stochastic/Labs/Support/Write_functions.h
+++ UrFU/Stochastic/Labs/Support/Write_functions.h
@@ -5,13 +5,56 @@
 #ifndef LABA1_WRITE_FUNCTIONS_H
 #define LABA1_WRITE_FUNCTIONS_H
 
-template<typename T>
-void write_vector_in_file(const std::vector<T> &v, const std::string &path, const bool clear_up = true);
+#include <fstream>
+#include <iomanip>
+#include "iostream"
 
 void write_two_vectors(const std::string &path, const std::vector<double> &data_set, const std::vector<int> &N_set);
 
+template<typename T>
+void write_vector_in_file(const std::vector<T> &v, const std::string &path, const bool clear_up = true)
+    {
+        if (clear_up)
+        {
+            std::ofstream out(path);
+            out.close();
+        }
+        std::ofstream out(path, std::ios::app);
+        if (out.fail())
+        {
+            std::cerr << "Can't reach or open file from this path: " << path << std::endl;
+            exit(-1);
+        }
+        if (!(v.empty()))
+        {
+            for (auto item : v)
+            {
+                out << std::setprecision(9) << item << " ";
+            }
+            out << '\n';
+        }
+        out.close();
+    }
 
-void write_number_in_file(const double n, const std::string &path);
+template<typename T>
+void write_number_in_file(const T n, const std::string &path)
+    {
+        std::ofstream out(path);
+        if (out.fail())
+        {
+            std::cerr << "Can't reach or open file from this path: " << path << std::endl;
+            exit(-1);
+        }
+        out << n;
+        out.close();
+    }
 
+template<typename T>
+std::string to_str(T const &value)
+    {
+        std::stringstream sstr;
+        sstr << value;
+        return sstr.str();
+    }
 
 #endif //LABA1_WRITE_FUNCTIONS_H
